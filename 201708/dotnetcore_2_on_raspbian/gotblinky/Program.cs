79c2443e1245a89bf968fa916f1963f282197d4d
Paul Amazona <netfiddler@yahoo.com>
2017-08-31 14:32:34 +0800

    added .net core 2.0 on Raspbian presentation

---
+++ 201708/dotnetcore_2_on_raspbian/gotblinky/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace gotblinky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Singapore .NET CORE Developers!!!");
+            Console.WriteLine("Let's do the blink!");
+            var x = 0;
+            while (true)
+            {
+                x = (x == 1) ? 0: 1;
+
+                // about to open pin 26 - test if it's already open
+                if (!Directory.Exists("/sys/class/gpio/gpio26"))
+                {
+                    Console.WriteLine("...about to open pin 26");
+                    File.WriteAllText("/sys/class/gpio/export", "26");
+                }
+                else
+                {
+                    Console.WriteLine("...pin is already open");
+                }
+
+                Console.WriteLine("...specifying direction of Pin 26 as OUT");
+                File.WriteAllText("/sys/class/gpio/gpio26/direction", "out");
+
+                Console.WriteLine("...setting output level to " + x.ToString());
+                File.WriteAllText("/sys/class/gpio/gpio26/value", x.ToString());
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ 201708/dotnetcore_2_on_raspbian/readme.md
@@ -0,0 +1,11 @@
+# .NET Core 2.0 on Raspbian
+material presented by Paul Lorett Amazona
+
+on 24Aug2017
+
+during the .NET Developers Singapore Meetup
+
+Event link:   
+https://www.meetup.com/preview/NET-Developers-SG/events/242551963   
+https://www.meetup.com/preview/AzureSG/events/242552048   
+
